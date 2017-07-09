97392023f2e177cfe8f42f8b734ecc607cf84add
Ho Yang <yeticry@gmail.com>
2017-07-09 20:10:40 +0800

    copy tabs url

--- common.js
+++ common.js
@@ -57,6 +57,31 @@ let openAllTabs = function() {
     return false;
 }
 
+let copyAllTabs = function() {
+    let saved_tabs = getAllSavesTabs();
+    var urls = "";
+    saved_tabs.forEach(function(tab) {
+        urls += tab.url + "\n";
+    });
+
+    if (window.clipboardData && window.clipboardData.setData) {
+        return clipboardData.setData("Text", urls);
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = urls;
+        textarea.style.position = "fixed";
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");
+        } catch (ex) {
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
 let openTab = function(event) {
     event.preventDefault();
     browser.tabs.create({ "url": event.target.href });
--- popup.js
+++ popup.js
@@ -3,13 +3,22 @@ var showTabsList = function(event) {
     var tabs_list = document.createElement("ul");
 
     var tab_item = document.createElement("li");
-    var tab_item_url = document.createElement("a");
-    tab_item_url.id = "open_all_tabs";
-    tab_item_url.href = "#";
-    tab_item_url.innerText = "OPEN ALL TABS";
-    tab_item_url.setAttribute("style", "text-align: center; font-weight: bold;");
-    tab_item.appendChild(tab_item_url);
+
+    var tab_item_open_all_tab = document.createElement("a");
+    tab_item_open_all_tab.id = "open_all_tabs";
+    tab_item_open_all_tab.href = "#";
+    tab_item_open_all_tab.innerText = "OPEN ALL TABS";
+    tab_item_open_all_tab.setAttribute("style", "float: left; text-align: center; font-weight: bold; width: 136px;");
+
+    var tab_item_copy_all_tab = document.createElement("a");
+    tab_item_copy_all_tab.id = "copy_all_tabs";
+    tab_item_copy_all_tab.href = "#";
+    tab_item_copy_all_tab.innerText = "COPY TABS URL";
+    tab_item_copy_all_tab.setAttribute("style", "float: left; text-align: center; font-weight: bold; width: 136px;");
+
     tabs_list.appendChild(tab_item);
+    tab_item.appendChild(tab_item_open_all_tab);
+    tab_item.appendChild(tab_item_copy_all_tab);
 
     saved_tabs.forEach(function(tab) {
         var tab_item = document.createElement("li");
@@ -27,6 +36,7 @@ var showTabsList = function(event) {
     container.appendChild(tabs_list);
 
     document.getElementById("open_all_tabs").addEventListener("click", openAllTabs);
+    document.getElementById("copy_all_tabs").addEventListener("click", copyAllTabs);
 };
 
 document.addEventListener("DOMContentLoaded", showTabsList);
