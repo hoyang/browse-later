d3a0798a9e02a742664d54e0efff2b896a443ee5
Ho Yang <yeticry@gmail.com>
2017-09-13 18:38:07 +0800

    add contextMenus for tab

--- background.js
+++ background.js
@@ -22,4 +22,27 @@ browser.browserAction.onClicked.addListener(function () {
     });
 });
 
+
+browser.menus.create({
+    id: browse_later_tab_menu_id,
+    title: "Add current tab to Browse-later",
+    contexts: ["tab"]
+});
+
+browser.menus.create({
+    id: browse_later_all_tab_menu_id,
+    title: "Add all tabs to Browse-later",
+    contexts: ["tab"]
+});
+
+browser.menus.onClicked.addListener((info, tab) => {
+    if(info.menuItemId == browse_later_tab_menu_id) {
+        saveTab(tab.id, tab.url, tab.title, tab.pinned, tab.favIconUrl);
+    } else if (info.menuItemId == browse_later_all_tab_menu_id) {
+        browser.tabs.query({currentWindow: true}).then((tabs) => {
+            saveTabs(tabs);
+        });
+    }
+});
+
 updateBrowserAction(); // update badge text and title
