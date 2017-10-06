ce119036166931ea6ab662e836fead874cad943c
Ho Yang <yeticry@gmail.com>
2017-10-06 13:02:36 +0800

    update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm ./browse-later.zip
-zip browse-later.zip icons/* LICENSE README.md background.js common.js manifest.json popup.js styles.css tabs_list.html
+zip -r browse-later.zip _locales icons LICENSE README.md background.js common.js manifest.json popup.js styles.css tabs_list.html
