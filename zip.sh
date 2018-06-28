d9d1b3275f79d40f77d051e7d835de5a6064fcde
H.Yang <yeticry@gmail.com>
2018-06-28 18:07:11 +0800

    beta version release, zip script update

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm ./browse-later.zip
-zip -x "*.DS_Store" -r browse-later.zip _locales icons LICENSE README.md background.js common.js manifest.json popup.js styles.css tabs_list.html
+zip -x "*.DS_Store" -r browse-later.zip _locales icons LICENSE README.md background.js common.js manifest.json popup.js styles photon pages tabs.js options.js tabs_list.html content_helper.js
